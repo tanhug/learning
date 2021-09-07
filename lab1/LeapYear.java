5f91fe0d8afd94b807c5bcb445cdbe956312d44d
tan <tan.hh@foxmail.com>
2021-09-07 21:20:26 +0800

    completed LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -13,6 +13,14 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+    /** Test whether or not a given year is a Leap Year. */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0)
+          return true;
+        if (year % 4 == 0 && year % 100 != 0)
+          return true;
+        return false;
+    }
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
